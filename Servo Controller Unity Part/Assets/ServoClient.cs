298ffe13b95c810297e6d70b27d7e66272ba60b0
Mario Gutierrez <0x4d4147@gmail.com>
2018-01-05 15:05:54 -0600

    unity part

---
+++ Servo Controller Unity Part/Assets/ServoClient.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System;
+
+public class ServoClient : MonoBehaviour
+{
+    int currentAngle = 100;
+    bool waitingForCommandToFinish = false;
+
+	public byte[] ipAddress;
+
+    Socket socket = null;
+
+    void Start()
+    {
+        Debug.Log("ServoClient:: Constructing socket.");
+        ConstructSocket();
+    }
+
+    void ConstructSocket()
+    {
+        try
+        {
+            int conPort = 80;
+			IPAddress hostAddress = new IPAddress(ipAddress);
+            IPEndPoint hostEndPoint = new IPEndPoint(hostAddress, conPort);
+
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(hostEndPoint);
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Error in ServoClient:: " + e.ToString());
+        }
+    }
+
+    void OnDisable()
+    {
+        socket.Close();
+    }
+
+    void SendInfo(string msg)
+    {
+        //Debug.LogFormat("ServoClient:: Attempting to send message {0}", msg);
+
+        // abort if already waiting for a command.
+        if (waitingForCommandToFinish)
+        {
+            Debug.Log("ServoClient:: Waiting for another command to finish.");
+            return;
+        }
+        waitingForCommandToFinish = true;
+
+        if (socket == null)
+        {
+            Debug.Log("ServoClient:: Socket doesn't exist, constructing socket.");
+            ConstructSocket();
+        }
+
+        if (socket == null)
+        {
+            Debug.Log("ServoClient:: Could not constuct socket.");
+        }
+
+        SocketAsyncEventArgs e = new SocketAsyncEventArgs();
+        byte[] msgBytes = Encoding.ASCII.GetBytes(msg);
+        e.SetBuffer(msgBytes, 0, msgBytes.Length);
+        e.Completed += (sender, f) => { waitingForCommandToFinish = false; };
+        socket.SendAsync(e);
+    }
+       
+    public void SetAngle(int angle)
+    {
+        SendInfo(angle.ToString());
+    }
+
+    public void TurnRight()
+    {
+        SetAngle(++currentAngle);
+    }
+
+    public void TurnLeft()
+    {
+        SetAngle(--currentAngle);
+    }
+}
---
+++ Servo Controller Unity Part/Assets/SphereServoController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereServoController : MonoBehaviour
+{
+    public ServoClient client;
+    public float cooldownSeconds = 0.01f;
+    Vector3 initialForwardDirection;
+
+    float lastCommandTime;
+
+    void Start()
+    {
+        initialForwardDirection = transform.forward;
+    }
+	
+	void Update()
+    {
+        ChangeAngle(Vector3.SignedAngle(initialForwardDirection, transform.forward, Vector3.up));
+	}
+
+    void ChangeAngle(float angleDeviation)
+    {
+        float currentTime = Time.time;
+        float elapsedTime = currentTime - lastCommandTime;
+        if (elapsedTime > cooldownSeconds)
+        {
+            int remappedAngle = (int)Mathf.Clamp(158.5f - angleDeviation, 67f, 250f);
+            lastCommandTime = currentTime;
+            client.SetAngle(remappedAngle);
+        }
+    }
+}
