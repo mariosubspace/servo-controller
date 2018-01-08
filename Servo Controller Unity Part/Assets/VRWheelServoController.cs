a2595e5e5d9ad9f9723d06772c7d69d2037d37d9
Mario Gutierrez <0x4d4147@gmail.com>
2018-01-08 14:53:00 -0600

    VR scene for Windows MR.

---
+++ Servo Controller Unity Part/Assets/VRWheelServoController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class VRWheelServoController : MonoBehaviour
+{
+    public ServoClient client;
+    public float cooldownSeconds = 0.01f;
+    Vector3 initialForwardDirection;
+
+    float lastCommandTime;
+
+    void Start()
+    {
+        initialForwardDirection = transform.TransformDirection(transform.forward);
+    }
+	
+	void Update()
+    {
+        ChangeAngle(Vector3.SignedAngle(initialForwardDirection, transform.TransformDirection(transform.forward), transform.TransformDirection(Vector3.up)));
+	}
+
+    void ChangeAngle(float angleDeviation)
+    {
+        float currentTime = Time.time;
+        float elapsedTime = currentTime - lastCommandTime;
+        if (elapsedTime > cooldownSeconds)
+        {
+            int remappedAngle = (int)Mathf.Clamp(158.5f - angleDeviation, 67f, 250f);
+            lastCommandTime = currentTime;
+            client.SetAngle(remappedAngle);
+        }
+    }
+}
